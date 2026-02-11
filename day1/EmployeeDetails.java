00168761beb953561fe9d3d382bfc47c6fc21271
krithikav-spec <krithiveeraraghavan@gmail.com>
2026-02-11 17:52:10 +0530

    Add files via upload

---
+++ day1/EmployeeDetails.java
@@ -0,0 +1,29 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "John doe";
+		int age = 30;
+		float salary = 55000.70f;
+		char gender = 'M';
+		boolean acceptTerms = true;
+		float rating = 4.5f;
+		long mobileNumber = 9876543210L;
+		int pinCode = 56001;
+		int luckyNumber = 7;
+		System.out.println("Employee Details");
+		System.out.println("Name: " + name);
+		System.out.println("Age: " + age);
+		System.out.println("Salary: " + salary);
+		System.out.println("Gender: " + gender);
+		System.out.println("AcceptTerms: " + acceptTerms);
+		System.out.println("Rating: " + rating);
+		System.out.println("MobileNumber: " + mobileNumber);
+		System.out.println("PinCode: " + pinCode);
+		System.out.println("LuckyNumber: " + luckyNumber);
+
+	}
+
+}
